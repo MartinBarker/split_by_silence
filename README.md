c3ad4ffb99c3ec8d29f974f4cb5158da7633c1cd
martin barker <martinbarker99@gmail.com>
2023-07-13 14:24:53 -0700

    dev

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
+#### NEW BELOW:
+
+./split_by_silence.sh "12 Tracks Donn Clayton Add A Manual Gourmet Touch.mp3" -numTracks 12
+
+#### OLD BELOW:: 
+
 Instructions:
 
 1) Get comma seperated string list of SPLIT points, add zero to start and total length to end
-`$ ./split_by_silence`
+`$ ./split_by_silence.sh "full_lowq.flac" %03d_output.flac`
 
 2) Convert list of split point times into a .cue file
 `$ ./readable_points_to_readable.sh`
 
 2) Use shntool to split the flac based on .cue input
 `$ shntool split -f times.cue -O always -o flac -d "%a - %t" -v -e 24/96 full.flac`
+
 
 
 
--- split_by_silence.sh
+++ split_by_silence.sh
@@ -1,77 +1,149 @@
+#!/bin/bash
 # -----------------------
 # SPLIT BY SILENCE
 # Requirements:
 #    ffmpeg
-#    $ apt-get install bc
+#    bc (apt-get install bc)
 # How To Run:
-# $ ./split_by_silence.sh "full_lowq.flac" %03d_output.flac
+# $ ./split_by_silence.sh <input_file> [-numTracks <target_num>]
 
-# output title format
-OUTPUTTITLE="%d_output.flac"
-# input audio filepath
-IN="/mnt/c/Users/marti/Documents/projects/split_by_silence2/full.flac"
-# output audio filepath
-OUTPUTFILEPATH="/mnt/c/Users/marti/Documents/projects/split_by_silence2"
+# Check if input file is provided
+if [ -z "$1" ]; then
+    echo "Usage: ./split_by_silence.sh <input_file> [-numTracks <target_num>]"
+    exit 1
+fi
+
+# Input audio filepath
+IN="$1"
+# Output folder path
+OUTPUTFOLDER="output"
+# Delete 'output' folder and its contents if it exists
+if [ -d "$OUTPUTFOLDER" ]; then
+    rm -rf "$OUTPUTFOLDER"
+fi
+# Create output folder
+mkdir -p "$OUTPUTFOLDER"
+# Output title format
+OUTPUTTITLE="$OUTPUTFOLDER/%03d_output.mp3"
+# Output audio filepath
+OUTPUTFILEPATH="$PWD/$OUTPUTTITLE"
 # ffmpeg option: split input audio based on this silencedetect value
 SD_PARAMS="-11dB"
 MIN_FRAGMENT_DURATION=120 # split option: minimum fragment duration
 export MIN_FRAGMENT_DURATION
 
+# Parse command-line arguments
+shift
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -numTracks)
+            NUM_TRACKS="$2"
+            shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Convert input file to MP3 format if not already converted
+if [ "${IN##*.}" != "mp3" ]; then
+    TEMP_FILE="${IN%.*}_mp3_converted.mp3"
+    if [ ! -f "$TEMP_FILE" ]; then
+        ffmpeg -i "$IN" -q:a 2 "$TEMP_FILE"
+    fi
+else
+    TEMP_FILE="$IN"
+fi
+
 # -----------------------
-# step: ffmpeg
-# goal: get comma separated list of split points (use ffmpeg to determine points where audio is at SD_PARAMS [-18db] )
+# Step: ffmpeg
+# Goal: Get comma-separated list of split points (use ffmpeg to determine points where audio is at SD_PARAMS [-18db] )
 
 echo "_______________________"
 echo "Determining split points..."
-SPLITS=$(
-    ffmpeg -v warning -i "$IN" -af silencedetect="$SD_PARAMS",ametadata=mode=print:file=-:key=lavfi.silence_start -vn -sn  -f s16le  -y /dev/null \
-    | grep lavfi.silence_start= \
-    | cut -f 2-2 -d= \
-    | perl -ne '
-        our $prev;
-        INIT { $prev = 0.0; }
-        chomp;
-        if (($_ - $prev) >= $ENV{MIN_FRAGMENT_DURATION}) {
-            print "$_,";
-            $prev = $_;
-        }
-    ' \
-    | sed 's!,$!!'
-)
-#echo "split points list= $SPLITS"
 
-# add '5.5' to each split (padding)
-IFS=',' read -ra SPLITS_ARRAY <<< "$SPLITS"
-for i in "${!SPLITS_ARRAY[@]}"; do
-  SPLITS_ARRAY[i]=$(echo "${SPLITS_ARRAY[i]}+5.5" | bc)
+# Function to check the number of tracks obtained for given SD_PARAMS and MIN_FRAGMENT_DURATION
+check_num_tracks() {
+    local splits="$1"
+    local num_tracks=$(IFS=','; echo "${#splits[@]}")
+    echo "$num_tracks"
+}
+
+# Function to run ffmpeg with given SD_PARAMS and MIN_FRAGMENT_DURATION
+run_ffmpeg() {
+    local splits="$1"
+    echo "Running ffmpeg with SD_PARAMS: $SD_PARAMS and MIN_FRAGMENT_DURATION: $MIN_FRAGMENT_DURATION"
+    ffmpeg -i "$TEMP_FILE" -c copy -map 0 -f segment -segment_times "$splits" "$OUTPUTFILEPATH"
+}
+
+# Initialize variables
+best_splits=()
+best_num_tracks=$((NUM_TRACKS + 1))
+
+# Loop through different combinations of SD_PARAMS and MIN_FRAGMENT_DURATION
+for db in -11 -12 -13; do
+    for duration in 120 150 180; do
+        SD_PARAMS="${db}dB"
+        MIN_FRAGMENT_DURATION="$duration"
+
+        # Determine split points
+        splits=$(
+            ffmpeg -v warning -i "$TEMP_FILE" -af silencedetect="$SD_PARAMS",ametadata=mode=print:file=-:key=lavfi.silence_start -vn -sn -f s16le -y /dev/null \
+            | grep lavfi.silence_start= \
+            | cut -f 2-2 -d= \
+            | perl -ne '
+                our $prev;
+                INIT { $prev = 0.0; }
+                chomp;
+                if (($_ - $prev) >= $ENV{MIN_FRAGMENT_DURATION}) {
+                    print "$_,";
+                    $prev = $_;
+                }
+            ' \
+            | sed 's!,$!!'
+        )
+
+        # Add '5.5' to each split (padding)
+        IFS=',' read -ra splits_array <<<"$splits"
+        for i in "${!splits_array[@]}"; do
+            splits_array[i]=$(echo "${splits_array[i]}+5.5" | bc)
+        done
+
+        # Check the number of tracks obtained
+        num_tracks=$(check_num_tracks "${splits_array[@]}")
+
+        # If the number of tracks is closest to the target, update the best splits and number of tracks
+        if [ "$num_tracks" -ge "$NUM_TRACKS" ] && [ "$num_tracks" -lt "$best_num_tracks" ]; then
+            best_splits=("${splits_array[@]}")
+            best_num_tracks=$num_tracks
+            best_params="SD_PARAMS: $SD_PARAMS, MIN_FRAGMENT_DURATION: $MIN_FRAGMENT_DURATION"
+        fi
+    done
 done
 
-SPLITS_DISPLAY=$SPLITS
-# add zero '0' to start o SPLITS_DISPLAY list
-SPLITS_DISPLAY="0,$SPLITS_DISPLAY"
+# Export tracks with best splits
+if [ ${#best_splits[@]} -eq 0 ]; then
+    echo "No suitable combination of SD_PARAMS and MIN_FRAGMENT_DURATION found to achieve the target number of tracks."
+else
+    echo "Best combination found:"
+    echo "$best_params"
 
-# get the full audio filelength for the input file $IN, and add that length (in seconds) to end of $SPLITS
-FILE_LENGTH=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$IN")
+    # Print out splits
+    splits_display=$(IFS=','; echo "${best_splits[*]}")
+    echo "SPLITS=$splits_display"
 
-# add file length to end of SPLITS
-SPLITS_DISPLAY="$SPLITS_DISPLAY,$FILE_LENGTH"
-echo "SPLITS_DISPLAY=$SPLITS_DISPLAY"
+    echo "_______________________"
+    echo "Exporting $best_num_tracks tracks with ffmpeg"
 
-# print out splits 
-SPLITS=$(IFS=','; echo "${SPLITS_ARRAY[*]}")
-echo "SPLITS=$SPLITS"
+    # Run ffmpeg with best splits
+    run_ffmpeg "${best_splits[@]}"
 
-# using the split points list, calculate how many output audio files will be created 
-num=0
-res="${SPLITS//[^,]}"
-CHARCOUNT="${#res}"
-num=$((CHARCOUNT + 2))
-echo "_______________________"
-echo "Exporting $num tracks with ffmpeg"
+    echo "Done."
+fi
 
-ffmpeg -i "$IN" -c copy -map 0 -f segment -segment_times "$SPLITS" "$OUTPUTFILEPATH/$OUTPUTTITLE"
-
-echo "Done."
+# Clean up temporary file
+if [ "$TEMP_FILE" != "$IN" ]; then
+    rm "$TEMP_FILE"
+fi
 
 echo "------------------------------------------------"
-echo "$num TRACKS EXPORTED"
+echo "$best_num_tracks TRACKS EXPORTED"
